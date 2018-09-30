33bbe8b210119b4883301e33dce1576052fe6edb
JooYooGit <jimmy.zhuyu@gmail.com>
2018-09-30 06:30:35 +0200

    mock data works

--- src/providers/card-service/card-service.ts
+++ src/providers/card-service/card-service.ts
@@ -17,23 +17,24 @@ export class CardServiceProvider {
 
 
   mockCardBages() {
+    //todo: to getDate when new card generate
     var date = new Date();
 
     var cardsA = [
-      new Card(0, date.getDate().toString(), '你好', 'hallo', CardStatus.failed),
-      new Card(1, date.getDate().toString(), '谢谢', 'danke', CardStatus.failed),
-      new Card(2, date.getDate().toString(), '再见', 'diedersehen', CardStatus.failed),
-      new Card(3, date.getDate().toString(), '对不起', 'entschuldigung', CardStatus.notSure),
-      new Card(4, date.getDate().toString(), '没关系', 'kein Problem', CardStatus.success)
+      new Card(0, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '你好', 'hallo', CardStatus.failed),
+      new Card(1, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '谢谢', 'danke', CardStatus.failed),
+      new Card(2, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '再见', 'diedersehen', CardStatus.failed),
+      new Card(3, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '对不起', 'entschuldigung', CardStatus.notSure),
+      new Card(4, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '没关系', 'kein Problem', CardStatus.success)
     ]
     var cardsB = [
-      new Card(4, date.getDate().toString(), '好的', 'ok', CardStatus.success),
-      new Card(4, date.getDate().toString(), '不是', 'nein', CardStatus.success),
-      new Card(4, date.getDate().toString(), '早上好', 'morgen', CardStatus.success),
-      new Card(4, date.getDate().toString(), '晚安', 'gute Nacht', CardStatus.success)
+      new Card(4, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '好的', 'ok', CardStatus.success),
+      new Card(4, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '不是', 'nein', CardStatus.success),
+      new Card(4, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '早上好', 'morgen', CardStatus.success),
+      new Card(4, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '晚安', 'gute Nacht', CardStatus.success)
     ]
     var cardsC = [
-      new Card(4, date.getDate().toString(), '好胃口', 'mahlzeit', CardStatus.success)
+      new Card(4, date.getUTCFullYear().toString() + ' / ' + date.getUTCMonth(), '好胃口', 'mahlzeit', CardStatus.success)
     ]
 
     this.cardBags = [
