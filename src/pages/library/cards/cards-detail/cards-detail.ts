db6e3536d0402df9d8749e6471f7c60d64d8e582
JooYoo <jooyoo@outlook.com>
2018-10-02 15:39:52 +0200

    optimized card detail page

--- src/pages/library/cards/cards-detail/cards-detail.ts
+++ src/pages/library/cards/cards-detail/cards-detail.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform, ViewController } from 'ionic-angular';
 
-
-
 @Component({
   selector: 'page-cards-detail',
   templateUrl: 'cards-detail.html',
 })
 export class CardsDetailPage {
 
-
-
   card: any
+  cardStatus: any
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -19,6 +16,9 @@ export class CardsDetailPage {
     public viewCtrl: ViewController) {
 
     this.card = navParams.get('cardInfo')
+
+    // get enum key
+    this.cardStatus = this.getCardStatus(this.card.status)
   }
 
 
@@ -28,8 +28,18 @@ export class CardsDetailPage {
   }
 
 
-
-
+  // todo: pack to service provider
+  getCardStatus(value: number) {
+    if (value == 0) {
+      return "success"
+    }
+    else if (value == 1) {
+      return "failed"
+    }
+    else if (value == 2) {
+      return "notSure"
+    }
+  }
 
 
 }
