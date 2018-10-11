8cf1c1f14266d2e2f34935883116a2182afaa3d9
JooYoo <jooyoo@outlook.com>
2018-10-11 17:19:18 +0200

    pass data to cube

--- src/pages/cube/cube.ts
+++ src/pages/cube/cube.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { CardServiceProvider } from '../../providers/card-service/card-service';
+import { SwipeServiceProvider } from '../../providers/swipe-service/swipe-service';
+import { Cube } from '../../app/Model/Cube';
 
 declare var Swiper
 @Component({
@@ -8,8 +11,22 @@ declare var Swiper
 })
 export class CubePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cubes: Cube[]
+  cube: Cube
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public cardService: CardServiceProvider,
+              public swipeService: SwipeServiceProvider) {
+
+                this.startNewRound(0)
   }
+
+startNewRound(cubeIndex: number){
+  let randomIndex = this.swipeService.getRandomCardBag(this.cardService.cubeBags.length)
+  this.cubes = this.cardService.cubeBags[randomIndex].cubes
+  this.cube = this.cubes[cubeIndex]
+}
 
   ngAfterViewInit(){
     var swiper = new Swiper('.swiper-container', {
@@ -26,7 +43,6 @@ export class CubePage {
         el: '.swiper-pagination',
       },
     });
-
   }
 
 }
