b94cc02d8a47b45ada2277ceccf846599eea755c
JooYooGit <jimmy.zhuyu@gmail.com>
2018-10-17 05:41:02 +0200

    play with foreach

--- src/pages/library/cube-list/cube-add/cube-add.ts
+++ src/pages/library/cube-list/cube-add/cube-add.ts
@@ -10,32 +10,54 @@ export class CubeAddPage {
 
   cubeBag: any
   sideLength: number = 3
-  sides:string[]
+  sides: string[]
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              public viewControl: ViewController,
-              public cardService: CardServiceProvider) {
-      this.cubeBag = navParams.get("cubeBagInfo")
-      this.sides = new Array(this.sideLength)
+  isEmptyArray: boolean = false;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewControl: ViewController,
+    public cardService: CardServiceProvider) {
+    this.cubeBag = navParams.get("cubeBagInfo")
+    this.sides = new Array(this.sideLength)
   }
 
-  addCube(){
+  addCube() {
     this.cardService.addCube(this.cubeBag, this.sides)
   }
 
-  onRangeChange(){
+  onRangeChange() {
 
-    
+    //TODO: case01: empty array
+    this.sides.forEach(side => {
+      console.log(this.isEmptyArray)
+
+      if (side == undefined) {
+        this.isEmptyArray = true
+        console.log(this.isEmptyArray)
+      }
+      this.isEmptyArray = false
+    });
+
+    if (this.isEmptyArray) {
       this.sides = new Array(this.sideLength)
-    
+
+    }
+
+    //TODO: case02: add new item in notEmpty array
+
+    //TODO: case03: reduce item in notEmpty array
+
+
+    console.log(this.sides)
+    console.log(this.sides[0])
   }
 
   trackByIndex(index: number, obj: any): any {
     return index;
   }
 
-  dismiss(){
+  dismiss() {
     this.viewControl.dismiss()
   }
 
