b391e91fbd31f6527761b9a75cbd4c080ab4914c
JooYoo <jooyoo@outlook.com>
2018-12-15 10:33:56 +0100

    CubeEditPage editCubeStack works

--- src/pages/library/cube-list/cube-list.ts
+++ src/pages/library/cube-list/cube-list.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams, ItemSliding, ModalController } from 'ionic-an
 import { CardServiceProvider } from '../../../providers/card-service/card-service';
 import { CubeEditPage } from './cube-edit/cube-edit';
 import { CubeAddPage } from './cube-add/cube-add';
+import { LibraryPage } from '../library';
 
 
 @Component({
@@ -12,30 +13,38 @@ import { CubeAddPage } from './cube-add/cube-add';
 export class CubeListPage {
 
   cubeBag: any
+  tabInfo: any
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              public modalControl: ModalController,
-              public cardService: CardServiceProvider) {
-      this.cubeBag = navParams.get('itemInfo')
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public modalControl: ModalController,
+    public cardService: CardServiceProvider) {
+
+    this.cubeBag = navParams.get('itemInfo')
+    this.tabInfo = "cube"
   }
 
   closeSlidingItem(slidingItem: ItemSliding) {
     slidingItem.close()
   }
 
-  openAddModal(){
+  openAddModal() {
     console.log("test")
-    let addModal = this.modalControl.create(CubeAddPage,{cubeBagInfo: this.cubeBag})
+    let addModal = this.modalControl.create(CubeAddPage, { cubeBagInfo: this.cubeBag })
     addModal.present()
   }
 
-  openEditModal(cube){
-    let editModal = this.modalControl.create(CubeEditPage,{cubeInfo: cube})
+  openEditModal(cube) {
+    let editModal = this.modalControl.create(CubeEditPage, { cubeInfo: cube })
     editModal.present()
   }
 
-  removeCube(cube){
-    this.cardService.removeCube(cube,this.cubeBag)
+  removeCube(cube) {
+    this.cardService.removeCube(cube, this.cubeBag)
+  }
+
+  editCubeBag() {
+    this.cardService.editCubeBag(this.cubeBag, this.cubeBag.titleCn, this.cubeBag.titleDe);
+    this.navCtrl.push(LibraryPage, {tabInfo: this.tabInfo});
   }
 }
--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,4 +1,4 @@
-import { Component,ChangeDetectionStrategy  } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NavController, NavParams, ModalController, ItemSliding, Platform } from 'ionic-angular';
 import { CardsPage } from "../library/cards/cards"
 import { CardServiceProvider } from '../../providers/card-service/card-service';
@@ -16,27 +16,35 @@ import { CubeBagAddPage } from '../library/cube-bag-add/cube-bag-add';
 
 export class LibraryPage {
 
-  libraryMode: string = "swipe"
+  libraryMode: string
   isAndroid: boolean = false
   cubeWrapperColor: string
+  tabInfo: any
 
   constructor(public nav: NavController,
-              public cardService: CardServiceProvider,
-              public modalControl: ModalController,
-              platform: Platform) {
+    public navParams: NavParams,
+    public cardService: CardServiceProvider,
+    public modalControl: ModalController,
+    platform: Platform) {
 
     this.isAndroid = platform.is('android')
-    this.libraryMode = "swipe"
+    this.tabInfo = navParams.get("tabInfo")
 
-    this.cubeWrapperColor = "#9C27B0"
+    // CubeStackEditPage: hit save Btn go back to CubeStackPage
+    if (this.tabInfo != undefined) {
+      this.libraryMode = this.tabInfo
+      return
+    }
+
+    this.libraryMode = "swipe"
   }
 
   // open specific card/cube Bag, display all cards or cubes
   openCardsPage(item) {
     this.nav.push(CardsPage, { itemInfo: item });
   }
-  openCubeListPage(item){
-    this.nav.push(CubeListPage, {itemInfo: item})
+  openCubeListPage(item) {
+    this.nav.push(CubeListPage, { itemInfo: item })
   }
 
   // right-top add button
@@ -44,7 +52,7 @@ export class LibraryPage {
     const AddModal = this.modalControl.create(CardBagAddPage)
     AddModal.present()
   }
-  openCubeBagAddPage(){
+  openCubeBagAddPage() {
     const AddModal = this.modalControl.create(CubeBagAddPage)
     AddModal.present()
   }
@@ -54,21 +62,20 @@ export class LibraryPage {
     const editModal = this.modalControl.create(CardBagEditPage, { itemInfo: item })
     editModal.present()
   }
-  openCubeBagEditPage(item){
-    const cubeEditModal = this.modalControl.create(CubeBagEditPage, {itemInfo:item})
+  openCubeBagEditPage(item) {
+    const cubeEditModal = this.modalControl.create(CubeBagEditPage, { itemInfo: item })
     cubeEditModal.present()
   }
 
   cardBagDelete(item) {
     this.cardService.removeCardBag(item)
   }
-  
-  cubeBagDelete(item){
-    console.log("inside cubeBagDelete")
+
+  cubeBagDelete(item) {
     this.cardService.removeCubeBag(item)
   }
 
-  getCubeStackColor(){
+  getCubeStackColor() {
     return this.cardService.getRandomBgColor();
   }
 
