6f9245bf491d3db6344eeace813787097cb777d3
JooYoo <jooyoo@outlook.com>
2018-12-15 16:29:43 +0100

    removed cube-bag-edit

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -4,7 +4,6 @@ import { CardsPage } from "../library/cards/cards"
 import { CardServiceProvider } from '../../providers/card-service/card-service';
 import { CardBagAddPage } from './card-bag-add/card-bag-add';
 import { CardBagEditPage } from './card-bag-edit/card-bag-edit';
-import { CubeBagEditPage } from '../library/cube-bag-edit/cube-bag-edit';
 import { CubeListPage } from '../../pages/library/cube-list/cube-list';
 import { CubeBagAddPage } from '../library/cube-bag-add/cube-bag-add';
 
@@ -63,10 +62,7 @@ export class LibraryPage {
     const editModal = this.modalControl.create(CardBagEditPage, { itemInfo: item })
     editModal.present()
   }
-  openCubeBagEditPage(item) {
-    const cubeEditModal = this.modalControl.create(CubeBagEditPage, { itemInfo: item })
-    cubeEditModal.present()
-  }
+ 
 
   cardBagDelete(item) {
     this.cardService.removeCardBag(item)
